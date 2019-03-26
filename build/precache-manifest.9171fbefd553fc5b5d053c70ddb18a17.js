038f9b18cc1d51a6d24e38861118a894d26d0d33
Svilen Nedev <svilen.nedev@mentormate.com>
2019-03-26 18:01:34 +0200

    Build folder is included

---
+++ build/precache-manifest.9171fbefd553fc5b5d053c70ddb18a17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2be9cbe18ef4bc72d96b",
+    "url": "/static/js/main.556eda29.chunk.js"
+  },
+  {
+    "revision": "cf8ca41b78aa01f8927a",
+    "url": "/static/js/2.9dfcb43f.chunk.js"
+  },
+  {
+    "revision": "2be9cbe18ef4bc72d96b",
+    "url": "/static/css/main.5d40cec8.chunk.css"
+  },
+  {
+    "revision": "b85f38987044d456134736a7c852b6d6",
+    "url": "/index.html"
+  }
+];
